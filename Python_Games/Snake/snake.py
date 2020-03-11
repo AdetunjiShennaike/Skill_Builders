5e02e5e6f5bf0a678c46725e662fee3eb3217207
Adetunji <Ashennaike@gmail.com>
2020-03-10 23:23:57 -0400

    snake setup complete

--- Python_Games/Snake/snake.py
+++ Python_Games/Snake/snake.py
@@ -5,11 +5,42 @@ import tkinter
 from tkinter import messagebox
 
 class cube(object):
+  def __init__(self, start, dirnx=1,dirny=0, color(255,0,0)):
+    pass
 
+  def move(self, dirnx, dirny):
+    pass
+
+  def draw(self, sureface, eyes=False):
+    pass
 
 class snake(object):
+  def __init__(self, color, pos):
+    pass
 
+  def move(self):
+    pass
 
-class drawGrid(w, rows, surface):
+  def reset(self,pos):
+    pass
 
+  def addCube(self):
+    pass
+  
+  def draw(self, surface):
+    pass
 
+def drawGrid(w, rows, surface):
+  pass
+
+def redrawWindow(surface):
+  pass
+
+def randomSnack(rows,items):
+  pass
+
+def message_box(subject, content):
+  pass
+
+def main():
+  pass
