f24cfada0b1f929ea84f00037c425f50c8334ea1
Adetunji <Ashennaike@gmail.com>
2020-03-15 00:58:42 -0400

    event handlers

--- Vue_js/index.js
+++ Vue_js/index.js
@@ -1,6 +1,7 @@
 var app = new Vue({
   // Options of the Vue instance
   el:'#app', //el = element property
+  // Data are things that can be put into the html file via referencing
   data: {
     product: 'Socks',
     image: './vmSocks-green-onWhite',
@@ -9,12 +10,25 @@ var app = new Vue({
     variants: [
       {
         variantID: 2234,
-        variantColor: 'green'
+        variantColor: 'green',
+        variantImage: './vmSocks-green-onWhite'
       },
       {
         variantID: 2235,
-        variantColor: 'blue'
+        variantColor: 'blue',
+        variantImage: './vmSocks-blue-onWhite'
       }
-    ]
+    ],
+    cart: 0
+  },
+  // Methods are used similar to class methods
+  methods: {
+    // Some browsers don't accept the add to cart format
+    addToCart() {
+      this.cart += 1
+    },
+    updateProduct: function(variantImage) {
+      this.image = variantImage
+    }
   }
 })
