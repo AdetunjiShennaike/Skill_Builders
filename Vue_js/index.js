27af1c8660a329fc9ddbeddac95e3ec3de687a34
Adetunji <Ashennaike@gmail.com>
2020-03-14 23:52:24 -0400

    renering lists, loops

--- Vue_js/index.js
+++ Vue_js/index.js
@@ -4,6 +4,17 @@ var app = new Vue({
   data: {
     product: 'Socks',
     image: './vmSocks-green-onWhite',
-    inventory: 100
+    inventory: 100,
+    details: ['80% cotton', '20% polyester', 'gender-neutral'],
+    variants: [
+      {
+        variantID: 2234,
+        variantColor: 'green'
+      },
+      {
+        variantID: 2235,
+        variantColor: 'blue'
+      }
+    ]
   }
 })
