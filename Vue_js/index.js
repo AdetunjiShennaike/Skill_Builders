959708943c11500bc8d0e9b74033289f93063028
Adetunji <Ashennaike@gmail.com>
2020-03-15 14:52:03 -0400

    computed properties

--- Vue_js/index.js
+++ Vue_js/index.js
@@ -3,21 +3,22 @@ var app = new Vue({
   el:'#app', //el = element property
   // Data are things that can be put into the html file via referencing
   data: {
+    brand: 'Naughty',
     product: 'Socks',
-    image: './vmSocks-green-onWhite',
-    inStock:true,
-    inventory: 100,
+    selectedVariant: 0,
     details: ['80% cotton', '20% polyester', 'gender-neutral'],
     variants: [
       {
         variantID: 2234,
         variantColor: 'green',
-        variantImage: './vmSocks-green-onWhite'
+        variantImage: './vmSocks-green-onWhite',
+        variantQuantity: 20
       },
       {
         variantID: 2235,
         variantColor: 'blue',
-        variantImage: './vmSocks-blue-onWhite'
+        variantImage: './vmSocks-blue-onWhite',
+        variantQuantity: 0
       }
     ],
     cart: 0
@@ -28,8 +29,22 @@ var app = new Vue({
     addToCart() {
       this.cart += 1
     },
-    updateProduct: function(variantImage) {
-      this.image = variantImage
+    updateProduct: function(index) {
+      this.selectedVariant = index
     }
+  },
+  // Cached value after the computation is done
+  computed: {
+    title() {
+      return this.brand + '' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
+    }
+    
   }
+
 })
