eb13324e4df8e6f19273c51638059e4e31f4b4d6
Adetunji <Ashennaike@gmail.com>
2020-03-17 22:38:34 -0400

    global components and slots

---
+++ Vue_js/real-world-ex/src/components/BaseIcon.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="icon-wrapper">
+    <svg class="icon" :width="width" :height="height">
+      <use v-bind="{ 'xlink:href':'/feather.svg#'+name }"/>
+    </svg>
+    <!-- You can use slots to insert information/data from any parent file that uses this component -->
+    <!-- Name slots if you are deciding to use multiple slots for a component -->
+    <slot name="eventData"></slot> <!-- You can add default data to be used, if you have a common response/data/object/text -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  margin-right: 6px;
+}
+</style>
--- Vue_js/real-world-ex/src/main.js
+++ Vue_js/real-world-ex/src/main.js
@@ -2,6 +2,32 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+// second way to import global component
+import BaseIcon from '@/components/BaseIcon.vue'
+
+// make a vue component with the imported component
+Vue.component('BaseIcon', BaseIcon)
+
+const requireComponent = require.context(
+  // First item is the directory to search, second item is if you want to search subdirectories, third is a regex of what to look for
+  './components',// the relative path of the directory to search
+  false,// subdirectories will not be searched
+  /Base[A-Z]\w+\.(vue|js)$/ // regular expression that searches for components starting with "Base" and ending in .vue or .js
+)
+
+requireComponent.keys().forEach(fileName => {
+  // .keys is an array of all file in the folder
+  const componentConfig = requireComponent(fileName)
+  // convert the filename, all to camel case
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '1')) // removes what's before and after the filename itself
+  )
+
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
+
 
 Vue.config.productionTip = false;
 
