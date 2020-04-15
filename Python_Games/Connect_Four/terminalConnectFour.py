a1329ac955b877830c935bec6cdd91a0bc677909
Adetunji <Ashennaike@gmail.com>
2020-04-14 22:41:57 -0400

    set up the input ask from player 1 and 2

--- Python_Games/Connect_Four/terminalConnectFour.py
+++ Python_Games/Connect_Four/terminalConnectFour.py
@@ -5,5 +5,20 @@ def create_board():
   board = numpy.zeros((6, 7))
   return board
 
+
+# Initiators, creating the board, starting the game, and setting the turn
 board = create_board()
-print(board)
+gameInProgress = True
+turn = 0
+
+while gameInProgress:
+  # Grab a move from Player 1
+  if turn % 2 == 0:
+    # By default input returns a string, wrap an int around it to get the number
+    selection = int(input(f'Make a move player 1(0-6):'))
+
+  # Grab a move from Player 2
+  if turn % 2 != 0:
+    selection = int(input(f'Your turn player 2(0-6):'))
+
+  turn += 1
