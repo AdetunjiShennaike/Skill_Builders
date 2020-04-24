1bcbe16a1177162214c6d0e881eec832b55d4665
Adetunji <Ashennaike@gmail.com>
2020-04-23 22:39:19 -0400

    setup global variables to begin

---
+++ Python_Games/Tetris/tetris.py
@@ -0,0 +1,14 @@
+import pygame
+import random
+
+# Global Variables
+# Screen size
+WIDTH = 800
+HEIGHT = 700
+# Game area size
+PLAY_WIDTH = 300
+PLAY_HEIGHT = 600
+BLOCK_SIZE = 30
+# Game area starting points
+X_AXIS = (WIDTH - PLAY_WIDTH) / 2
+Y_AXIS = (HEIGHT - PLAY_HEIGHT) /2
