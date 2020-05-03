75af8ba608718d67cfc01fcb1079b22df4ff8322
Adetunji <Ashennaike@gmail.com>
2020-05-02 23:04:09 -0400

    basic global variables set

---
+++ Python_Games/MultiplayerGame/multi.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+
+# Global Variables
+WIDTH = 500
+HEIGHT = 500
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Client")
+
+clientNumber = 0
+
