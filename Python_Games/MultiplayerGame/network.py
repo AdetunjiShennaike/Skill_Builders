5b00b2bc7560007300abb9b7a606523e86c10b1a
Adetunji <Ashennaike@gmail.com>
2020-05-10 22:46:30 -0400

    adding comments and network file more complete

--- Python_Games/MultiplayerGame/network.py
+++ Python_Games/MultiplayerGame/network.py
@@ -1,6 +1,7 @@
 import socket
 
-
+# Network class is responsible for connecting to the server
+# Having it as a new file allows for it to be a reusable class/component
 class Network:
   def __init__(self):
     self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,5 +15,11 @@ class Network:
     try:
       self.client.connect(self.address)
       return self.client.recv(2048).decode()
-    except:
-      pass
+    except socket.error as err:
+      print(err)
+
+net = Network()
+print(net.send('hello'))
+print(net.send('working'))
+
+  
